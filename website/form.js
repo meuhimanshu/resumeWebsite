e7fe2c5f1e1946a5f656dfc77bf12b585297680a
Himanshu Singh <63249620+meuhimanshu@users.noreply.github.com>
2024-11-22 11:25:20 -0800

    newphoneChanges

--- website/form.js
+++ website/form.js
@@ -1,21 +1,21 @@
 function validateForm() {
-    let fname = document.forms["myform"]["first_name"].value;
-    let lname = document.forms["myform"]["last_name"].value;
-    let phone= document.forms["myform"]["phone_number"].value;
-    let text;
-    const phonePattern = /^[0-9]{10}$/;
-    if (fname == "") {
+  let fname = document.forms["myform"]["first_name"].value;
+  let lname = document.forms["myform"]["last_name"].value;
+  let phone = document.forms["myform"]["phone_number"].value;
+  let text;
+  const phonePattern = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+  
+  if (fname == "") {
       alert("First name must be filled out");
       return false;
-    } else if (lname==""){
-        alert("Last name must be filled out");
-        return false;
-    }
-
-    if (!phonePattern.test(phone)) {
-        text = "Telephone must be filled out like this: 555-555-5555";
-        document.getElementById("demo").innerHTML = text;
-        return false;
-    }
-
+  } else if (lname == "") {
+      alert("Last name must be filled out");
+      return false;
   }
+
+  if (!phonePattern.test(phone)) {
+      text = "Telephone must be filled out like this: 555-555-5555";
+      document.getElementById("demo").innerHTML = text;
+      return false;
+  }
+}
