1babd17a2eb3b3741ad4f1a7bdb26ccfcf847d0f
Himanshu Singh <63249620+meuhimanshu@users.noreply.github.com>
2024-12-02 19:44:48 -0800

    newPAGES

--- website/form.js
+++ website/form.js
@@ -1,15 +1,12 @@
 function validateForm() {
-  // Fetching form values
   let fname = document.forms["myform"]["first_name"].value;
   let lname = document.forms["myform"]["last_name"].value;
   let phone = document.forms["myform"]["phone_number"].value;
   let email = document.forms["myform"]["email"].value;
 
-  // Validation patterns
   const phonePattern = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  // Validation logic
   if (fname.trim() === "") {
       alert("First name must be filled out");
       return false;
@@ -29,9 +26,6 @@ function validateForm() {
       document.getElementById("demo").innerHTML = "Telephone must be filled out like this: 555-555-5555";
       return false;
   }
-
-
-
-  // If all validations pass
+  
   return true;
 }
